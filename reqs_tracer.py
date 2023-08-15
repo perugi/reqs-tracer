da63321361bdded63e6fec898b0a59db17958f56
Dominik Peruško <dominik.perusko@gmail.com>
2023-08-15 21:33:21 +0200

    Add export to excel and support for a section to cover multiple requirements and a single requirement to be covered in multiple sections

--- reqs_tracer.py
+++ reqs_tracer.py
@@ -1,5 +1,6 @@
 from docx import Document
 import re
+from openpyxl import Workbook
 
 req_id_pattern = r"SYSREQ-\d+"
 
@@ -15,7 +16,7 @@ for paragraph in reqs_doc.paragraphs:
 
 
 def get_heading_number(paragraph):
-    heading_regex = r"^((\d+\.)+) [\w ]+"
+    heading_regex = r"^((\d+\.)+) [\w\(\)\:\- ]+"
     # heading_regex = r"^((\d+\.)+).*"
     match = re.search(heading_regex, paragraph.text)
     if match:
@@ -30,16 +31,28 @@ for paragraph in arc_doc.paragraphs:
     heading_number = get_heading_number(paragraph)
     if heading_number:
         current_heading = heading_number
+        print(current_heading)
 
-    match = re.search(req_id_pattern, paragraph.text)
-    if match:
-        arc_reqs[match.group()] = current_heading
-
-# print(source_reqs)
-# print(arc_reqs)
+    matches = re.findall(req_id_pattern, paragraph.text)
+    if matches:
+        for match in matches:
+            arc_reqs[match] = arc_reqs.setdefault(match, []) + [current_heading]
 
 trace_table = dict()
 for source_req in source_reqs:
-    trace_table[source_req] = arc_reqs.setdefault(source_req, "Requirement not covered")
+    trace_table[source_req] = arc_reqs.setdefault(
+        source_req, ["Requirement not covered"]
+    )
 
-print(trace_table)
+trace_table = dict(sorted(trace_table.items()))
+
+# print(trace_table)
+
+wb = Workbook()
+ws = wb.active
+
+ws.append(["Requirement UID", "Section in Architecture"])
+for key, value in trace_table.items():
+    ws.append([key, ", ".join(value)])
+
+wb.save("trace.xlsx")
