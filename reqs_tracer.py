472d788c11f2a3588772a8c5d82600540268ae88
Dominik Peruško <dominik.perusko@gmail.com>
2023-08-17 15:38:24 +0200

    Listing of total and untraced requirements.

--- reqs_tracer.py
+++ reqs_tracer.py
@@ -14,6 +14,9 @@ for paragraph in reqs_doc.paragraphs:
     if match:
         source_reqs.append(match.group())
 
+# print(source_reqs)
+print(f"Number of source requirements: {len(source_reqs)}")
+
 
 def get_heading_number(paragraph):
     heading_regex = r"^((\d+\.)+) [\w\(\)\:\- ]+"
@@ -31,22 +34,29 @@ for paragraph in arc_doc.paragraphs:
     heading_number = get_heading_number(paragraph)
     if heading_number:
         current_heading = heading_number
-        print(current_heading)
+        # print(current_heading)
 
     matches = re.findall(req_id_pattern, paragraph.text)
     if matches:
         for match in matches:
             arc_reqs[match] = arc_reqs.setdefault(match, []) + [current_heading]
 
+# print(arc_reqs)
+
 trace_table = dict()
+untraced = []
 for source_req in source_reqs:
-    trace_table[source_req] = arc_reqs.setdefault(
-        source_req, ["Requirement not covered"]
-    )
+    if source_req not in arc_reqs:
+        untraced.append(source_req)
+        trace_table[source_req] = ["Requirement not covered"]
+    else:
+        trace_table[source_req] = arc_reqs[source_req]
 
 trace_table = dict(sorted(trace_table.items()))
 
 # print(trace_table)
+print(f"Number of untraced requirements: {len(untraced)}")
+print(f"Untraced: {', '.join(untraced)}")
 
 wb = Workbook()
 ws = wb.active
